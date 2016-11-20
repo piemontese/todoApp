8d8e14a24c3c78418b26bda4e7b15a15c4a07baa
= <pie.piemontese@gmail.com>
2016-11-20 01:03:18 +0100

    add data service

--- src/app/save-dialog/save-dialog.component.ts
+++ src/app/save-dialog/save-dialog.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from '../data.service';
+import { Subscription }   from 'rxjs/Subscription';
 import { Todo } from '../todo';
 
 @Component({
   selector: 'app-save-dialog',
   templateUrl: './save-dialog.component.html',
-  styleUrls: ['./save-dialog.component.css']
+styleUrls: ['./save-dialog.component.css']
 })
 export class SaveDialogComponent implements OnInit {
   @Input() todoListName: string = '';
+  subscription: Subscription;
+  todoList = Array();
 
   model = new Todo(); 
 
-  constructor() { }
+  constructor( private dataService: DataService ) { 
+    this.subscription = dataService.todoListName$.subscribe(
+      todoListName => {
+        this.todoListName = todoListName;
+        console.log('SaveDialog.todoListName = ' + this.todoListName);
+    });
+    /*
+    this.subscription = dataService.todoList$.subscribe(
+      todoList => {
+        this.todoList = todoList;
+        console.log('SaveDialog.todoList = ' + this.todoList);
+    });
+    */
+  }
   
   show() { 
   }
@@ -22,6 +39,7 @@ export class SaveDialogComponent implements OnInit {
 
   okClick() { 
     console.log("OK");
+    this.dataService.setTodoListName(this.model.todoListName);
   }
 
   ngOnInit() {
